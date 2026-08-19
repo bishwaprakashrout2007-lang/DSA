16859072f843b4a3da5bebadaf70e6215f86d80e
Bishwa prakash Rout <bishwaprakashrout2007@gmail.com>
2026-08-19 23:03:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (88.86%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        n = len(obstacleGrid[0])
+        dp = [0] * n
+
+        dp[0] = 1
+
+        for row in obstacleGrid:
+            for j in range(n):
+                if row[j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j - 1]
+
+        return dp[-1]
